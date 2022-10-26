2d9b95811f140c6c0a4d73b59e05b3a3d42dc263
itzjac <isaacvegan@gmail.com>
2022-10-26 11:31:48 -0500

    Pong Review 2

--- Pong/Assets/Scripts/ExitManager.cs
+++ Pong/Assets/Scripts/ExitManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ExitManager : MonoBehaviour
 {
+    // NOTE(isaveg): Belongs to the PongGameManager, no extra class for this functionality needed
     public void Exit()
     {
         Application.Quit();
--- Pong/Assets/Scripts/FPSLimitator.cs
+++ Pong/Assets/Scripts/FPSLimitator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FPSLimitator : MonoBehaviour
 {
+    // NOTE(isaveg): Belongs to the PongGameManager, no extra class for this functionality needed
     // Start is called before the first frame update
     void Start()
     {
